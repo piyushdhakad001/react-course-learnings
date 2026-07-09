9f7ff1a27d88d466c765aa245ca4686b153854f0
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-07-10 05:05:19 +0530

    Generate html for the cart items

--- 07-backend-async-await/learnings/ecommerce-project/src/pages/HomePage.jsx
+++ 07-backend-async-await/learnings/ecommerce-project/src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
+import { formatMoney } from '../utils/money';
 import "./HomePage.css";
 
 
@@ -45,7 +46,7 @@ export function HomePage( {cart} ) {
                   <div className="product-rating-count link-primary">{product.rating.count}</div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">${formatMoney(product.priceCents / 100).toFixed(2)}</div>
 
                 <div className="product-quantity-container">
                   <select>
