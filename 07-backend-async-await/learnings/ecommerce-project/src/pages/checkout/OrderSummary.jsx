ce40329e761f16a22937519ab22fca13fb633993
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-07-10 22:55:01 +0530

    Move cart details to a seperate component

--- 07-backend-async-await/learnings/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ 07-backend-async-await/learnings/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs';
-import { formatMoney } from '../../utils/money';
 import { DeliveryOptions } from './DeliveryOptions';
+import { CartDetails } from './CartItemDetails';
 
 
 export function OrderSummary({ cart, deliveryOptions }) {
@@ -18,28 +18,8 @@ export function OrderSummary({ cart, deliveryOptions }) {
             </div>
 
             <div className="cart-item-details-grid">
-              <img
-                className="product-image"
-                src={cartItem.product.image}
-              />
-
-              <div className="cart-item-details">
-                <div className="product-name">
-                  {cartItem.product.name}
-                </div>
-                <div className="product-price">{formatMoney(cartItem.product.priceCents)})</div>
-                <div className="product-quantity">
-                  <span>
-                    Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                  </span>
-                  <span className="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span className="delete-quantity-link link-primary">
-                    Delete
-                  </span>
-                </div>
-              </div>
+              
+              <CartDetails cartItem={cartItem} />
 
               <DeliveryOptions cartItem={cartItem} deliveryOptions={deliveryOptions} />
             </div>
